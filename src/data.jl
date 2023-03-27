4dc4f3d74275d794fff26619191b8925066d05be
Jungsoo Kim <jungsu93@gmail.com>
2023-03-27 10:03:24 -0400

    add witvliet connectome

--- src/data.jl
+++ src/data.jl
@@ -1,10 +1,15 @@
 # connectome
 const path_connectome_white = joinpath(@__DIR__, "..", "data", "white_1986_whole.json")
 const data_connectome_white = JSON.parsefile(path_connectome_white, dicttype=Dict, inttype=Int64)
+const path_connectome_witvliet_7 = joinpath(@__DIR__, "..", "data", "witvliet_2020_7.json")
+const data_connectome_witvliet_7 = JSON.parsefile(path_connectome_witvliet_7, dicttype=Dict, inttype=Int64)
+const path_connectome_witvliet_8 = joinpath(@__DIR__, "..", "data", "witvliet_2020_8.json")
+const data_connectome_witvliet_8 = JSON.parsefile(path_connectome_witvliet_8, dicttype=Dict, inttype=Int64)
 
 # 2d embedding data
 const path_connectome_plot = joinpath(@__DIR__, "..", "data", "connectome_diagram_pos.h5")
 const path_connectome_p_plot = joinpath(@__DIR__, "..", "data", "connectome_diagram_pharynx_pos.h5")
+
 const dict_pos_z_non_p = h5read(path_connectome_plot, "z")
 const dict_pos_v2_non_p = h5read(path_connectome_plot, "v2")
 const dict_pos_v3_non_p = h5read(path_connectome_plot, "v3")
