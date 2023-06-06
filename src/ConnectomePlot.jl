f033f549f78fa12007076ba4fbf975326bc3c2be
Adam Atanas <adamatanas@gmail.com>
2023-06-06 17:55:55 -0400

    Add additional connectome significance tests

--- src/ConnectomePlot.jl
+++ src/ConnectomePlot.jl
@@ -8,23 +8,28 @@ include("data.jl")
 include("graph.jl")
 include("plot.jl")
 include("table.jl")
+include("significance.jl")
 
-export get_neuron_type_wh_dvlr,
+export 
+    # graph.jl
+    get_neuron_type_wh_dvlr,
     get_node_name,
     get_graph_white,
     get_graph_white_p,
     get_graph_witvliet,
     get_sensory_muscle,
-    # graph.jl
+    # plot.jl
     color_connectome,
     color_connectome_kde,
     color_connectome_multi_kde,
-    test_signifiance_connectome,
     # data.jl
     get_dict_pos_patched,
     # table.jl
     generate_check_categorization,
     get_tau,
     neuropal_aggregate_data,
-    neuropal_count
+    neuropal_count,
+    # significance.jl
+    test_signifiance_connectome,
+
 end # module ConnectomePlot
